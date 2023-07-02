e22e4097581d70d8940002a84e7921b7fe6c312d
Ruchir-Bhatia <bhatiaruchir04@gmail.com>
2023-07-03 03:25:25 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,34 +6,7 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-
-const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
+import { ServiceCard } from "../components/ServiceCard";
 
 const Hero = () => {
   const isMobile = window.matchMedia("(max-width: 500px)").matches;
@@ -56,32 +29,31 @@ const Hero = () => {
             I am a Web Developer and a Learner.
           </p>
         </div>
-
-        <ServiceCard key={services[0].title} index={1} {...services[0]} />
-        {/* <ComputersCanvas /> */}
-
-        {isMobile && (
-          <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-            <a href='#about'>
-              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-                <motion.div
-                  animate={{
-                    y: [0, 24, 0],
-                  }}
-                  transition={{
-                    duration: 1.5,
-                    repeat: Infinity,
-                    repeatType: "loop",
-                  }}
-                  className='w-3 h-3 rounded-full bg-secondary mb-1'
-                />
-              </div>
-            </a>
-          </div>
-        )}
       </div>
 
-      
+      {/* <ComputersCanvas /> */}
+
+      {isMobile && (
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+          <a href='#about'>
+            <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+              <motion.div
+                animate={{
+                  y: [0, 24, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className='w-3 h-3 rounded-full bg-secondary mb-1'
+              />
+            </div>
+          </a>
+        </div>
+      )}
+
+      <ServiceCard key={services[0].title} index={1} {...services[0]} />
     </section>
   );
 };
