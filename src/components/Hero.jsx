6917bd60eabf6b202cd39985aadb65a94cb70434
Ruchir-Bhatia <bhatiaruchir04@gmail.com>
2023-07-03 03:07:27 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { ServiceCard } from "../components/ServiceCard";
 
 const Hero = () => {
   const isMobile = window.matchMedia("(max-width: 500px)").matches;
@@ -24,29 +29,31 @@ const Hero = () => {
             I am a Web Developer and a Learner.
           </p>
         </div>
+
+        {/* <ComputersCanvas /> */}
+
+        {isMobile && (
+          <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+            <a href='#about'>
+              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+                <motion.div
+                  animate={{
+                    y: [0, 24, 0],
+                  }}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }}
+                  className='w-3 h-3 rounded-full bg-secondary mb-1'
+                />
+              </div>
+            </a>
+          </div>
+        )}
       </div>
 
-      {/* <ComputersCanvas /> */}
-
-      {isMobile && (
-        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-          <a href='#about'>
-            <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-              <motion.div
-                animate={{
-                  y: [0, 24, 0],
-                }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: "loop",
-                }}
-                className='w-3 h-3 rounded-full bg-secondary mb-1'
-              />
-            </div>
-          </a>
-        </div>
-      )}
+      <ServiceCard key={services[0].title} index={1} {...services[0]} />
     </section>
   );
 };
