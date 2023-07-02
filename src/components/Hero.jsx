193ef398646ea73a4f49c85d804765e7d6440d01
Ruchir-Bhatia <bhatiaruchir04@gmail.com>
2023-07-03 02:42:52 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,30 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-full h-full object-contain'
+        />
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
 const Hero = () => {
   const isMobile = window.matchMedia("(max-width: 500px)").matches;
 
@@ -26,6 +50,11 @@ const Hero = () => {
         </div>
       </div>
 
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
       {/* <ComputersCanvas /> */}
 
       {isMobile && (
